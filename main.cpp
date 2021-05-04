270bb4bc3c29d0356afde73f3ea0a5cf97f3fc4c
spacetimeengineer3 <erwibben@gmail.com>
2021-05-04 16:13:45 +0000

    basic funcationality

--- main.cpp
+++ main.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
-#include "Boot.h"
+#include<vector>
+//#include "Boot.h"
+//#include "People.h"
 using namespace std;
-//Use '-' instead of '_'
+//Use '_' instead of '-'
+//
+class Firefighter{
+	private:
+
+	public:
+		string name;
+		string lastLoc;
+		Firefighter(string first, string last, vector<string> &all){
+			name = first + " " + last;
+			all.push_back(name);
+		}
+		void move(vector<string> &locaiton){
+			locaiton.push_back(name);
+		}
+			
+			
+};
+
+void listAll(vector<string> &location){
+for (int i = 0; i < location.size(); i++){
+		cout << location[i] <<  endl;
+}
+
+}
 int main(){
 	cout << "Hello Commander!" <<endl;
-	boot();
-
+	vector<string> all;
+	vector<string> cold;
+	vector<string> warm;
+	vector<string> hot;
+	all.push_back ("Incident Commander");
+	Firefighter John("John", "Smith", all);
+	Firefighter Dude ("Dude", "Lebowski", all);
+	John.add(cold);
+	Dude.add(cold);
+	listAll(cold);
 
 
 
